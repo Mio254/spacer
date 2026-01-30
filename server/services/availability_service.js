a4ab7eb0c7897dc8126ca84cf7583d3e61c7c72b
josephine anyango <anyangojozey@gmail.com>
2026-01-30 19:47:02 +0300

    updated services

---
+++ server/services/availability_service.js
@@ -0,0 +1,12 @@
+import Booking from "../models/booking.js"
+
+export const isAvailable = async (spaceId, startDate, endDate) => {
+  const clash = await Booking.findOne({
+    space_id: spaceId,
+    start_date: { $lt: endDate },
+    end_date: { $gt: startDate },
+    status: { $ne: "cancelled" },
+  })
+
+  return !clash
+}
