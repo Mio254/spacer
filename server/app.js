13204f8a6a622622b55116bcd0db3324fc2c1158
josephine anyango <anyangojozey@gmail.com>
2026-02-03 10:47:54 +0300

    commit

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 import express from "express"
-import bookingRoutes from "./routes/booking_routes.js"
+import bookingRoutes from "./app/routes/booking_routes.js"
 
 const app = express()
+const port = 5000
 
 app.use(express.json())
 app.use("/api/bookings", bookingRoutes)
 
-export default app
+app.get("/", (req, res) => {
+  res.json({ status: "server running" })
+})
+
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`)
+})
