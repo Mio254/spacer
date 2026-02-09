02fafc6bda7962f32c63a5ced24b4857d717874b
Abdifatah <mursalabdifatah17@gmail.com>
2026-02-09 10:41:35 +0300

    space-model-Abdifatah

--- client/src/services/spaceService.js
+++ client/src/services/spaceService.js
@@ -6,8 +6,11 @@ export const spaceService = {
     return await res.json();
   },
   async getSpace(id) {
+    console.log('Fetching spaces from:', `${API_URL}/spaces`);
     const res = await fetch(`${API_URL}/spaces/${id}`);
     if (!res.ok) throw new Error('Failed fetch space');
     return await res.json();
   }
 };
+
+
